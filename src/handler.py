72b66409347c72ce11f4e341ac84a4874238d7e8
Ryan Semmler <ryan.semmler@gmail.com>
2020-10-27 16:45:20 -0400

    standardize some inputs from csv

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,7 @@
 import json
 import os
+import csv
+import datetime
 from pymongo import MongoClient
 
 db_uri = os.environ.get("MONGO_DB_URI", "localhost")
@@ -15,7 +17,49 @@ def handle_csv_upload(event, context):
         "errors": [],
     }
 
-    # YOUR LOGIC HERE
+    # get users from event
+    event_data = csv.DictReader(event.splitlines())
+
+    new_keys = {
+        'Email': 'normalized_email',
+        'Manager': 'manager_id',
+        'Hire Date': 'hire_date',
+    }
+
+    for user in event_data:
+        # standardize data
+        user_data = {}
+        for k, v in user.items():
+            k = new_keys[k] if k in new_keys else k.lower()
+            if k == 'salary':
+                v = int(v)
+            elif k == 'hire_date':
+                v = datetime.datetime.strptime(v, '%m/%d/%Y')
+            user_data[k] = v
+
+
+
+        # user_data = {new_keys[k] if k in new_keys else k.lower(): v for k, v in user.items()}
+
+        # check whether user already exists in db
+        existing_user = db.user.find_one({'normalized_email': user_data['normalized_email']})
+
+        if existing_user:
+            # update existing user
+            db.user.update_one({'normalized_email': user_data['normalized_email']},
+                               {"$set": user_data})
+            response_body['numUpdated'] += 1
+        else:
+            # create new user
+            db.user.insert_one(user_data)
+            response_body['numCreated'] += 1
+
+        # # update field names
+        # translation = {'Email': 'normalized_email'}
+        # update = {"$rename": translation}
+        # db.user.update({'Email': user['Email']}, update)
+
+        import pdb; pdb.set_trace()
 
     response = {
         "statusCode": 200,
